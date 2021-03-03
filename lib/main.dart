094299c14c476006df7bdb6f9f2b94d5d5cb76b3
AdamProbert <adamprobert@live.co.uk>
2021-03-03 13:31:55 +0000

    Trying to add a wheat image

--- lib/main.dart
+++ lib/main.dart
@@ -54,13 +54,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
     });
+    // This call to setState tells the Flutter framework that something has
+    // changed in this State, which causes it to rerun the build method below
+    // so that the display can reflect the updated values. If we changed
+    // _counter without calling setState(), then the build method would not be
+    // called again, and so nothing would appear to happen.
+    _counter++;
   }
 
   @override
@@ -97,6 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Image(image: AssetImage('assets/Wheat.jpg')),
             Text(
               'You have pushed the button this many times:',
             ),
